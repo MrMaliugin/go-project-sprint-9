a7c8d2ba523f89b8a9ff75020394ad278019264a
Egor Maliugin <egor.malyugin71@gmail.com>
2024-04-23 16:53:32 +0300

    new

--- precode.go
+++ precode.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -13,20 +14,34 @@ import (
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
 	// 1. Функция Generator
-	// ...
+	defer close(ch)
+
+	for i := int64(1); ; i++ {
+		select {
+		case <-ctx.Done():
+			return
+		case ch <- i:
+			fn(i)
+		}
+	}
+
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
 	// 2. Функция Worker
-	// ...
+	defer close(out)
+	for i := range in {
+		out <- i
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
 
 	// 3. Создание контекста
-	// ...
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -36,6 +51,7 @@ func main() {
 	go Generator(ctx, chIn, func(i int64) {
 		inputSum += i
 		inputCount++
+
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -55,7 +71,16 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// ...
+	for i, out := range outs {
+		wg.Add(1)
+		go func(in <-chan int64, i int64) {
+			defer wg.Done()
+			for value := range in {
+				amounts[i]++
+				chOut <- value
+			}
+		}(out, int64(i))
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -68,11 +93,14 @@ func main() {
 	var sum int64   // сумма чисел результирующего канала
 
 	// 5. Читаем числа из результирующего канала
-	// ...
+	for val := range chOut {
+		count++
+		sum += val
+	}
 
-	fmt.Println("Количество чисел", inputCount, count)
-	fmt.Println("Сумма чисел", inputSum, sum)
-	fmt.Println("Разбивка по каналам", amounts)
+	fmt.Println("Количество чисел:", inputCount, count)
+	fmt.Println("Сумма чисел:", inputSum, sum)
+	fmt.Println("Разбивка по каналам:", amounts)
 
 	// проверка результатов
 	if inputSum != sum {
